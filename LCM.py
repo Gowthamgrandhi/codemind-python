47e314021f524efe013acfd69d71893f663283e9
Gowthamgrandhi <97425819+Gowthamgrandhi@users.noreply.github.com>
2022-06-30 21:43:47 +0530

    n,m=map(int,input().split()) for i in range(2,n*m+1):     if i%n==0 and i%m==0:         print(i)         break

--- LCM.py
+++ LCM.py
@@ -1,5 +1,5 @@
-a,b=map(int,input().split())
-for i in range(1,a*b+1):
-    if i%a==0 and i%b==0:
+n,m=map(int,input().split())
+for i in range(2,n*m+1):
+    if i%n==0 and i%m==0:
         print(i)
-        break;
+        break
